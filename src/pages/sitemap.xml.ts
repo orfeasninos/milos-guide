95bd6b27e1e65d5f4879713d81e11da532163cc0
orfeasninos <orfeasninos5@gmail.com>
2026-07-13 19:02:25 +0300

    404 fix

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -9,34 +9,22 @@ const SITE = 'https://milos.guide';
 const today = new Date().toISOString().split('T')[0];
 
 const statics = [
-  { en: '/',              el: '/el/',              priority: '1.0' },
-  { en: '/beaches/',      el: '/el/beaches/',      priority: '0.9' },
-  { en: '/villages/',     el: '/el/villages/',     priority: '0.9' },
-  { en: '/sights/',       el: '/el/sights/',       priority: '0.9' },
-  { en: '/map/',          el: '/el/map/',          priority: '0.7' },
-  { en: '/how-to-reach/', el: '/el/how-to-reach/', priority: '0.7' },
-  { en: '/weather/',      el: '/el/weather/',      priority: '0.6' },
-  { en: '/about/',        el: '/el/about/',        priority: '0.6' },
-  { en: '/search/',       el: '/el/search/',       priority: '0.5' },
-  { en: '/contact/',      el: '/el/contact/',      priority: '0.4' },
-  { en: '/privacy/',      el: '/el/privacy/',      priority: '0.3' },
+  { en: '/',              priority: '1.0' },
+  { en: '/beaches/',      priority: '0.9' },
+  { en: '/villages/',     priority: '0.9' },
+  { en: '/sights/',       priority: '0.9' },
+  { en: '/map/',          priority: '0.7' },
+  { en: '/how-to-reach/', priority: '0.7' },
+  { en: '/weather/',      priority: '0.6' },
+  { en: '/about/',        priority: '0.6' },
+  { en: '/search/',       priority: '0.5' },
+  { en: '/contact/',      priority: '0.4' },
+  { en: '/privacy/',      priority: '0.3' },
 ];
 
-function urlPair(en: string, el: string, priority: string) {
+function urlPair(en: string, priority: string) {
   return `  <url>
     <loc>${SITE}${en}</loc>
-    <xhtml:link rel="alternate" hreflang="x-default" href="${SITE}${en}"/>
-    <xhtml:link rel="alternate" hreflang="en" href="${SITE}${en}"/>
-    <xhtml:link rel="alternate" hreflang="el" href="${SITE}${el}"/>
-    <lastmod>${today}</lastmod>
-    <changefreq>daily</changefreq>
-    <priority>${priority}</priority>
-  </url>
-  <url>
-    <loc>${SITE}${el}</loc>
-    <xhtml:link rel="alternate" hreflang="x-default" href="${SITE}${en}"/>
-    <xhtml:link rel="alternate" hreflang="en" href="${SITE}${en}"/>
-    <xhtml:link rel="alternate" hreflang="el" href="${SITE}${el}"/>
     <lastmod>${today}</lastmod>
     <changefreq>daily</changefreq>
     <priority>${priority}</priority>
@@ -45,11 +33,11 @@ function urlPair(en: string, el: string, priority: string) {
 
 export const GET: APIRoute = () => {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-${statics.map(s    => urlPair(s.en, s.el, s.priority)).join('\n')}
-${beachesData.map(b  => urlPair(`/beaches/${b.slug}/`,  `/el/beaches/${b.slug}/`,  '0.8')).join('\n')}
-${villagesData.map(v => urlPair(`/villages/${v.slug}/`, `/el/villages/${v.slug}/`, '0.8')).join('\n')}
-${sightsData.map(s   => urlPair(`/sights/${s.slug}/`,   `/el/sights/${s.slug}/`,   '0.7')).join('\n')}
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${statics.map(s    => urlPair(s.en, s.priority)).join('\n')}
+${beachesData.map(b  => urlPair(`/beaches/${b.slug}/`,  '0.8')).join('\n')}
+${villagesData.map(v => urlPair(`/villages/${v.slug}/`, '0.8')).join('\n')}
+${sightsData.map(s   => urlPair(`/sights/${s.slug}/`,   '0.7')).join('\n')}
 </urlset>`;
 
   return new Response(xml, {
